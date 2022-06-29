806e69f49686924ae87fbebe52c0c72a764d1d90
verazhen <vera.zhen63@gmail.com>
2022-06-29 22:57:23 +0800

    <api> inspect the input and response with corresponding http status code

--- src/scooter/scooter.controller.ts
+++ src/scooter/scooter.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Query,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { ScooterService } from './scooter.service';
 import { Scooter } from './scooter.entity';
 
@@ -7,8 +16,23 @@ export class ScooterController {
   constructor(private readonly scooterService: ScooterService) {}
 
   @Get()
-  async getAll(@Query('x') x, @Query('y') y, @Query('radius') radius) {
-    return this.scooterService.getAll(x, y, radius);
+  async getAll(
+    @Query('x') x,
+    @Query('y') y,
+    @Query('radius') radius,
+    @Res() res: Response,
+  ) {
+    if (!x || !y || !radius) {
+      return res.status(HttpStatus.UNAUTHORIZED).json({
+        status_code: HttpStatus.UNAUTHORIZED,
+        message: 'query is missing',
+      });
+    }
+    //how to deal with input 0
+    return res.status(HttpStatus.OK).json({
+      status_code: HttpStatus.OK,
+      data: await this.scooterService.getAll(x, y, radius),
+    });
   }
 
   @Post()
